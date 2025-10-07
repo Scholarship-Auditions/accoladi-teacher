5044843ed51e66c3df820e76e2ce1c4f8e736981
SadamRohith <accoladi.tech@gmail.com>
2025-10-07 16:19:36 -0500

    Linked Aj's Neubert Profile

--- src/app/layout/pages/home/home.ts
+++ src/app/layout/pages/home/home.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { GotCovered } from '../../components/got-covered/got-covered';
+import { Component } from "@angular/core";
+import { GotCovered } from "../../components/got-covered/got-covered";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  imports:[GotCovered],
-  templateUrl: './home.html',
-  styleUrl: './home.scss'
+  selector: "app-home",
+  imports: [GotCovered, RouterModule],
+  templateUrl: "./home.html",
+  styleUrl: "./home.scss",
 })
-
-export class Home {
-
-}
+export class Home {}
