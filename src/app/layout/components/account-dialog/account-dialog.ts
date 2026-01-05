b25ec941d2acb8bc596315dac21141b331035c1c
SadamRohith <sadamrohith456@gmail.com>
2026-01-05 17:48:07 -0600

    Added hamburger menu and updated the dailog

--- src/app/layout/components/account-dialog/account-dialog.ts
+++ src/app/layout/components/account-dialog/account-dialog.ts
@@ -1,26 +1,24 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
+import { Component } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-account-dialog',
+  selector: "app-account-dialog",
   standalone: true,
   imports: [MatIconModule],
-  templateUrl: './account-dialog.html',
-  styleUrl: './account-dialog.scss'
+  templateUrl: "./account-dialog.html",
+  styleUrl: "./account-dialog.scss",
 })
 export class AccountDialog {
-
-  constructor(public dialogRef: MatDialogRef<AccountDialog>) { }
+  constructor(public dialogRef: MatDialogRef<AccountDialog>) {}
 
   close(): void {
     this.dialogRef.close();
   }
 
   continue(): void {
-    this.dialogRef.close('continue');
+    this.dialogRef.close("continue");
     //open this url in new tab
-    window.open('https://my.accoladi.com/auth/register/student', '_blank');
+    window.open("https://my.accoladi.com/auth/register/teacher", "_blank");
   }
-
 }
